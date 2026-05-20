d6a424d90b3cd75967ee80f186933745163f1a49
Jordan W <112892092+GMSplash@users.noreply.github.com>
2026-05-20 08:26:50 -0500

    update 1
    Added in a more structured variable grouping.
    Added in class bonuses v1
    added in base values for several things
    finished first room loop

--- Py.py
+++ Py.py
@@ -1,4 +1,30 @@
+#Imports:
 import random
+
+#Character information:
+char_name = ""
+char_class = "Peasant"
+char_level = 1
+char_exp = 0
+
+#Character stats:
+char_health = 10
+char_mana = 5
+char_str = 0
+char_dex = 0
+char_int = 0
+char_wis = 0
+char_con = 0
+
+#Character Spells:
+
+#Character Skills: 
+
+#Inventory:
+gold = 0
+current_weapon = "Unarmed"
+weapon_stash = []
+
 
 char_name = input("Enter your name: ")
 
@@ -13,50 +39,74 @@ class_choice = input("Enter the number of your chosen class: ")
 
 if class_choice == "1":
     char_class = "Warrior"
+    char_str += 5
+    char_con += 3
 
 elif class_choice == "2":
     char_class = "Mage"
+    char_int += 5
+    char_wis += 3
+    mana_bonus_roll = random.randint(1,20)
+    char_mana += mana_bonus_roll + char_wis
 
 elif class_choice == "3":
     char_class = "Rogue"
+    char_str += 3
+    char_dex += 5
 
 elif class_choice == "4":
     char_class = "Archer"
+    char_dex += 5
+    char_wis += 3
+    char_mana += char_wis
+
 
 elif class_choice == "5":
     char_class = "Cleric"
+    char_wis += 5
+    char_con += 3
+    mana_bonus_roll = random.randint(1,20)
+    char_mana += mana_bonus_roll + char_wis
 
 else:
     char_class = "Peasant"
+    char_str += 1
+    char_dex += 1
+    char_int += 1
+    char_wis += 1
+    char_con += 1
+    char_mana += char_wis
 
-roll1 = random.randint(1, 20)
-roll2 = random.randint(1, 20)
+HP_roll1 = random.randint(1, 10)
+HP_roll2 = random.randint(1, 10)
 
-health = roll1 + roll2 - 7
+char_health += HP_roll1 + HP_roll2 + char_con
 
-gold = 0
 
 print(f"{char_class} {char_name} enters the dungeon with {health} HP! In front of you expands a large entryway. There is a door infront of you and to your left." )
 
-print("What do you do?")
-print("1. Move Forward")
-print("2. Move Left")
-print("3. Loot the room")
+move_choice = 99
 
-move_choice = input("Type the number associated with the action you want to preform")
+while move_choice >= 4:
+    print("What do you do?")
+    print("1. Move Forward")
+    print("2. Move Left")
+    print("3. Loot the room")
 
-if move_choice == "1":
-    print("You move to the door infront of you. It is a large oaken door with a brass knob. Beyond the door you hear the sound of a fire and low murmurs you can't quite make out.")
+    move_choice = int(input("Type the number associated with the action you want to preform: "))
 
-elif move_choice == "2":
-   print("You move to the left. Blocking your way is a small wrought iron gate. The latch is open. You could push it open easily.")
+    if move_choice == 1:
+        print("You move to the door infront of you. It is a large oaken door with a brass knob. Beyond the door you hear the sound of a fire and low murmurs you can't quite make out.")
 
-elif move_choice == "3":
-    roll_loot = random.randint(1, 100)
-    gold += roll_loot
-    print(f"You look around the room and find a leather pouch. When you pick it up you hear the jingle of coins. Upon opening you find {roll_loot} gold. You now have {gold} gold.")
+    elif move_choice == 2:
+        print("You move to the left. Blocking your way is a small wrought iron gate. The latch is open. You could push it open easily.")
 
-else:
-    move_choice == ""
+    elif move_choice == 3:
+        roll_loot = random.randint(1, 100)
+        gold += roll_loot
+        print(f"You look around the room and find a leather pouch. When you pick it up you hear the jingle of coins. Upon opening you find {roll_loot} gold. You now have {gold} gold.")
 
-print(move_choice)
+    else:
+        print("Please use a valid choice. ")
+
+print("You died!")
